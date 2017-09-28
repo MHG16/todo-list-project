dd3028fb99319d04589674d039b2c958442f93e9
Martin Grossmann <mhgrossmann@yahoo.com>
2017-09-28 14:42:41 -0400

    Added strike-through functionality

--- Assets/js/todos.js
+++ Assets/js/todos.js
@@ -1,5 +1,30 @@
 // Check off specific Todos by clicking
 $("li").click(function() {
-	$(this).css("color", "gray");
-	$(this).css("text-decoration", "line-through");
+	//if li is gray
+	if($(this).css("color")  === "rgb(128, 128, 128)") {
+			//turn it black
+		$(this).css({
+
+			//turn it black
+			color: "black",
+			textDecoration: "none"
+
+		});
+	}	
+	//else
+	else {
+		$(this).css({
+
+			//turn it gray
+			color: "gray",
+			textDecoration: "line-through"
+		});
+
+
+	}
+
+		
+
+
+
 });
